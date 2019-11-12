b84084c14f8900ac564ba1ea4baccd27db276f9e
Hitoshi Kurokawa <hitoshi@nextseed.jp>
2019-11-12 23:06:40 +0900

    add Register REGISTER_NAME

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,25 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::path::Path;
 
+enum Register {
+    EAX = 0, ECX = 1, EDX = 2, EBX =3,
+    ESP = 4, EBP = 5, ESI = 6, EDI = 7
+}
+
 struct Emulator {
     memory: Vec<u8>,
     eip: usize,
+    register: [u32; 8]
 }
+
+static REGISTER_NAME: [&str; 8] =
+ ["EAX", "ECX", "EDX", "EBX", "ESP", "EBP", "ESI", "EDI"];
 
 fn main() {
     let mut emu = Emulator {
         memory: Vec::new(),
-        eip: 0
+        eip: 0,
+        register: [0; 8]
     };
 
     let path = Path::new("test1");
@@ -27,10 +37,14 @@ fn main() {
         emu.eip += 1;
         println!("opcode: {:2X}", code);
 
-        if code == 0xb8 {
-            println!("mov eax,?");
+        if (0xb8 <= code) && (code <= (0xb8 + 7)) {
+            let reg = (code - 0xb8) as usize;
+            let reg_name = REGISTER_NAME[reg];
+            println!("reg: {}", REGISTER_NAME[reg]);
+            println!("mov {},?", reg_name);
             let value = code32(&mut emu, 0);
-            println!("mov eax,{:#X}", value);
+            println!("mov {},{:#X}",reg_name,  value);
+            emu.register[reg] = value;
             emu.eip += 4;
         } else if code == 0xc3 {
             println!("ret");
@@ -50,6 +64,7 @@ fn code8(emu: &Emulator, index: usize) -> u32 {
 
 fn code32(emu: &Emulator, index: usize) -> u32 {
     let mut value: u32 = 0;
+    let mut data: String = String::new();
 
     let mut count = 0;
     loop {
@@ -58,6 +73,8 @@ fn code32(emu: &Emulator, index: usize) -> u32 {
         }
 
         let mut temp = code8(emu, index + count);
+        let str = format!("{:2X}", temp);
+        data.push_str(&str);
         println!("hex: {:2X}", temp);
         println!("bin: {:032b}", temp);
         temp <<= 8 * count;
@@ -66,6 +83,7 @@ fn code32(emu: &Emulator, index: usize) -> u32 {
 
         count += 1;
     }
+    println!("data: {}", data);
 
     return value;
 }
