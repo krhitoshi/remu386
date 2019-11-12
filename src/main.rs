985fdafc999eca22d9cd3e93c5a5ecc31428ce69
Hitoshi Kurokawa <hitoshi@nextseed.jp>
2019-11-12 21:29:48 +0900

    add code32

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,13 @@ use std::path::Path;
 
 struct Emulator {
     memory: Vec<u8>,
-    epi: usize,
+    eip: usize,
 }
 
 fn main() {
-    let emu = Emulator {
+    let mut emu = Emulator {
         memory: Vec::new(),
-        epi: 0
+        eip: 0
     };
 
     let path = Path::new("test1");
@@ -20,67 +20,62 @@ fn main() {
         Ok(f) => f,
     };
 
-    let mut eip = 0;
+    f.read_to_end(&mut emu.memory);
 
-    let mut buffer = Vec::new();
-    f.read_to_end(&mut buffer);
-
-    let code = code8(&buffer, eip);
+    let code = code8(&mut emu, 0);
     println!("opcode: {:2X}", code);
     if code == 0xb8 {
         println!("mov eax,?");
     }
+    emu.eip += 1;
 
-    eip += 1;
+    let value = code32(&mut emu, 0);
+    println!("value: {}", value);
+    emu.eip += 4;
 
+    let code = code8(&mut emu, 0);
+    println!("opcode: {:2X}", code);
+    if code == 0xc3 {
+        println!("ret");
+    }
+    emu.eip += 1;
+}
+
+fn code8(emu: &Emulator, index: usize) -> u32 {
+    return emu.memory[emu.eip + index].into();
+}
+
+fn code32(emu: &Emulator, index: usize) -> u32 {
     let mut value: u32 = 0;
 
     let mut temp: u32 = 0;
-    temp = buffer[eip] as u32;
-    println!("hex: {:2X}", buffer[eip]);
-    println!("bin: {:032b}", buffer[eip]);
+    temp = code8(emu, 0);
+    println!("hex: {:2X}", temp);
+    println!("bin: {:032b}", temp);
 
     value = temp;
     println!("value: {}", value);
-    eip += 1;
 
-    temp = buffer[eip] as u32;
-    value += (temp << 8);
-    println!("hex: {:2X}", buffer[eip]);
-    println!("bin: {:032b}", buffer[eip]);
+    temp = code8(emu, 1);
+    value += temp << 8;
+    println!("hex: {:2X}", temp);
+    println!("bin: {:032b}", temp);
     println!("bin: {:032b}", temp << 8);
     println!("value: {}", value);
-    eip += 1;
 
-    temp = buffer[eip] as u32;
-    value += (temp << 16);
-    println!("hex: {:2X}", buffer[eip]);
-    println!("bin: {:032b}", buffer[eip]);
+    temp = code8(emu, 2);
+    value += temp << 16;
+    println!("hex: {:2X}", temp);
+    println!("bin: {:032b}", temp);
     println!("bin: {:032b}", temp << 16);
     println!("value: {}", value);
-    eip += 1;
 
-    temp = buffer[eip] as u32;
-    value += (temp << 24);
-    println!("hex: {:2X}", buffer[eip]);
-    println!("bin: {:032b}", buffer[eip]);
+    temp = code8(emu, 3);
+    value += temp << 24;
+    println!("hex: {:2X}", temp);
+    println!("bin: {:032b}", temp);
     println!("bin: {:032b}", temp << 25);
     println!("value: {}", value);
-
-    println!("value: {:032b}", value);
-    eip += 1;
-
-    println!("opcode: {:2X}", buffer[eip]);
-    if buffer[eip] == 0xc3 {
-        println!("ret");
-    }
-    eip += 1;
-
-    // for byte in f.bytes() {
-    //     println!("{:#04x}", byte.unwrap());
-    // }
+    return value;
 }
 
-fn code8(buffer: &Vec<u8>, index: usize) -> u32 {
-    return buffer[index].into();
-}
