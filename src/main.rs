fb4de19ed6832da6a9df60b7efece1fba2e3a3fc
Hitoshi Kurokawa <hitoshi@nextseed.jp>
2019-11-12 21:42:27 +0900

    use loop

--- src/main.rs
+++ src/main.rs
@@ -22,23 +22,22 @@ fn main() {
 
     f.read_to_end(&mut emu.memory);
 
-    let code = code8(&mut emu, 0);
-    println!("opcode: {:2X}", code);
-    if code == 0xb8 {
-        println!("mov eax,?");
-    }
-    emu.eip += 1;
+    loop {
+        let code = code8(&mut emu, 0);
+        emu.eip += 1;
+        println!("opcode: {:2X}", code);
 
-    let value = code32(&mut emu, 0);
-    println!("value: {}", value);
-    emu.eip += 4;
-
-    let code = code8(&mut emu, 0);
-    println!("opcode: {:2X}", code);
-    if code == 0xc3 {
-        println!("ret");
+        if code == 0xb8 {
+            println!("mov eax,?");
+            let value = code32(&mut emu, 0);
+            println!("mov eax,{:#X}", value);
+            emu.eip += 4;
+        } else if code == 0xc3 {
+            println!("ret");
+            println!("break");
+            break;
+        }
     }
-    emu.eip += 1;
 }
 
 fn code8(emu: &Emulator, index: usize) -> u32 {
@@ -48,34 +47,22 @@ fn code8(emu: &Emulator, index: usize) -> u32 {
 fn code32(emu: &Emulator, index: usize) -> u32 {
     let mut value: u32 = 0;
 
-    let mut temp: u32 = 0;
-    temp = code8(emu, 0);
-    println!("hex: {:2X}", temp);
-    println!("bin: {:032b}", temp);
+    let mut count = 0;
+    loop {
+        if count > 3 {
+            break;
+        }
 
-    value = temp;
-    println!("value: {}", value);
+        let mut temp = code8(emu, index + count);
+        println!("hex: {:2X}", temp);
+        println!("bin: {:032b}", temp);
+        temp <<= 8 * count;
+        println!("bin: {:032b}", temp);
+        value += temp;
 
-    temp = code8(emu, 1);
-    value += temp << 8;
-    println!("hex: {:2X}", temp);
-    println!("bin: {:032b}", temp);
-    println!("bin: {:032b}", temp << 8);
-    println!("value: {}", value);
+        count += 1;
+    }
 
-    temp = code8(emu, 2);
-    value += temp << 16;
-    println!("hex: {:2X}", temp);
-    println!("bin: {:032b}", temp);
-    println!("bin: {:032b}", temp << 16);
-    println!("value: {}", value);
-
-    temp = code8(emu, 3);
-    value += temp << 24;
-    println!("hex: {:2X}", temp);
-    println!("bin: {:032b}", temp);
-    println!("bin: {:032b}", temp << 25);
-    println!("value: {}", value);
     return value;
 }
 
