9dbb54d1e0eed840056aed9a1002076edc5784cf
Hitoshi Kurokawa <hitoshi@nextseed.jp>
2019-11-16 16:27:00 +0900

    support sign flag

--- src/emulator.rs
+++ src/emulator.rs
@@ -293,16 +293,25 @@ impl Emulator {
     }
 
     fn cmp_rm32_imm8(&mut self, modrm: ModRM) {
-        let value = self.code8(0);
+        let value = self.code8(0) as i64;
         self.epi_inc();
         let reg_name = self.register_name(modrm.rm);
         println!("cmp {},{}", reg_name, value);
         println!("eflags = {:032b}", self.eflags);
-        let reg_value = self.register[modrm.rm as usize];
-        if reg_value == value {
+        let mut result = self.register[modrm.rm as usize] as i64;
+        result -= value;
+        println!("result {}", result);
+        // ZF: Zero Flag
+        if result == 0 {
             self.eflags |= 0b1000000;
         } else {
             self.eflags &= 0b11111111111111111111111110111111;
+        }
+        // SF: Sign Flag
+        if (result & 0b10000000000000000000000000000000) == 0b10000000000000000000000000000000 {
+            self.eflags |= 0b10000000;
+        } else {
+            self.eflags &= 0b11111111111111111111111101111111;
         }
         println!("eflags = {:032b}", self.eflags);
     }
