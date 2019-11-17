d1545ae1c9aea1691434efeef916ebc0ce6c1797
Hitoshi Kurokawa <hitoshi@nextseed.jp>
2019-11-17 19:56:51 +0900

    ModRM::new takes code8

--- src/emulator.rs
+++ src/emulator.rs
@@ -23,13 +23,34 @@ struct ModRM {
 }
 
 impl ModRM {
-    pub fn new() -> ModRM {
+    pub fn new(code: u32) -> ModRM {
         let mut modrm = ModRM {
             mode: 0,
             reg: 0,
             rm: 0,
             opcode: 0
         };
+
+        println!("ModR/M: {:02X} {:#8b}", code, code);
+        let mod_mask = 0b11000000;
+        modrm.mode = (code & mod_mask) >> 6;
+
+        let reg_mask = 0b00111000;
+        modrm.reg = (code & reg_mask) >> 3;
+        modrm.opcode = modrm.reg;
+
+        let rm_mask = 0b00000111;
+        modrm.rm = code & rm_mask;
+
+        // let reg_name1 = self.register_name(modrm.reg);
+        // let reg_name2 = self.register_name(modrm.rm);
+
+        // println!("Mod: {:02b}, REG: {:03b} (opcode: {}, {}), R/M: {:03b} ({})",
+        //          modrm.mode, modrm.reg, modrm.opcode, reg_name1,
+        //          modrm.rm, reg_name2);
+        println!("Mod: {:02b}, REG: {:03b} (opcode: {}), R/M: {:03b}",
+                 modrm.mode, modrm.reg, modrm.opcode, modrm.rm);
+
         return modrm;
     }
 }
@@ -167,32 +188,7 @@ impl Emulator {
     fn read_modrm(&mut self) -> ModRM {
         let code = self.code8(0);
         self.epi_inc();
-        println!("ModR/M: {:02X} {:#8b}", code, code);
-
-        let mut modrm = ModRM {
-            mode: 0,
-            reg: 0,
-            rm: 0,
-            opcode: 0
-        };
-
-        let mod_mask = 0b11000000;
-        modrm.mode = (code & mod_mask) >> 6;
-
-        let reg_mask = 0b00111000;
-        modrm.reg = (code & reg_mask) >> 3;
-        modrm.opcode = modrm.reg;
-
-        let rm_mask = 0b00000111;
-        modrm.rm = code & rm_mask;
-
-        let reg_name1 = self.register_name(modrm.reg);
-        let reg_name2 = self.register_name(modrm.rm);
-
-        println!("Mod: {:02b}, REG: {:03b} (opcode: {}, {}), R/M: {:03b} ({})",
-                 modrm.mode, modrm.reg, modrm.opcode, reg_name1,
-                 modrm.rm, reg_name2);
-        return modrm;
+        return ModRM::new(code);
     }
 
     fn read_sib(&mut self) -> SIB {
@@ -783,7 +779,9 @@ mod tests {
     use super::ModRM;
     #[test]
     fn modrm_new() {
-        let mut modrm = ModRM::new();
-        assert_eq!(modrm.mode, 0);
+        let modrm = ModRM::new(0x61);
+        assert_eq!(modrm.mode, 0b01);
+        assert_eq!(modrm.reg, 0b100);
+        assert_eq!(modrm.rm, 0b001);
     }
 }
