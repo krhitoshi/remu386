df7e1dd998ada4058096a06980d0dc1ac1b833be
Hitoshi Kurokawa <hitoshi@nextseed.jp>
2019-11-17 20:21:08 +0900

    add modrm.rs

--- src/emulator.rs
+++ src/emulator.rs
@@ -1,3 +1,5 @@
+mod modrm;
+use modrm::ModRM;
 use Register::*;
 
 // 1MB 0x00000 - 0xfffff
@@ -13,46 +15,6 @@ pub struct Emulator {
     eip: u32,
     register: [u32; 8],
     eflags: u32
-}
-
-struct ModRM {
-    mode: u32,
-    reg: u32,
-    rm: u32,
-    opcode: u32
-}
-
-impl ModRM {
-    pub fn new(code: u32) -> ModRM {
-        let mut modrm = ModRM {
-            mode: 0,
-            reg: 0,
-            rm: 0,
-            opcode: 0
-        };
-
-        println!("ModR/M: {:02X} {:#8b}", code, code);
-        let mod_mask = 0b11000000;
-        modrm.mode = (code & mod_mask) >> 6;
-
-        let reg_mask = 0b00111000;
-        modrm.reg = (code & reg_mask) >> 3;
-        modrm.opcode = modrm.reg;
-
-        let rm_mask = 0b00000111;
-        modrm.rm = code & rm_mask;
-
-        // let reg_name1 = self.register_name(modrm.reg);
-        // let reg_name2 = self.register_name(modrm.rm);
-
-        // println!("Mod: {:02b}, REG: {:03b} (opcode: {}, {}), R/M: {:03b} ({})",
-        //          modrm.mode, modrm.reg, modrm.opcode, reg_name1,
-        //          modrm.rm, reg_name2);
-        println!("Mod: {:02b}, REG: {:03b} (opcode: {}), R/M: {:03b}",
-                 modrm.mode, modrm.reg, modrm.opcode, modrm.rm);
-
-        return modrm;
-    }
 }
 
 struct SIB {
@@ -771,17 +733,5 @@ impl Emulator {
             let value = self.register[i];
             println!("{} = {:#010X} {}", reg_name, value, value);
         }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::ModRM;
-    #[test]
-    fn modrm_new() {
-        let modrm = ModRM::new(0x61);
-        assert_eq!(modrm.mode, 0b01);
-        assert_eq!(modrm.reg, 0b100);
-        assert_eq!(modrm.rm, 0b001);
     }
 }
